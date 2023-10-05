7182764358bedcd0ae3c696e3b5eea6a3b157372
Adrielle Bueno <drihbueno22@gmail.com>
2023-10-05 11:25:38 -0300

    Alterações finais

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -11,13 +11,20 @@ describe('Funcionalidade Pré Cadastro', () => {
     });
           
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
-        cy.get('#reg_password').type(faker.internet.password())
+
+        let nomefaker = faker.name.firstName()
+        let sobrenomefaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomefaker,sobrenomefaker)
+        let passwordFaker = faker.internet.password()
+      
+
+        cy.get('#reg_email').type(emailFaker)
+        cy.get('#reg_password').type(passwordFaker)
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nomefaker)
+        cy.get('#account_last_name').type(sobrenomefaker)
         cy.get('.woocommerce-Button').click()
         
         cy.get('.woocommerce-message').should('contain', 'modificados')
