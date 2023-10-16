0db8687f1692a700405b6cc8ac1979829824968e
Adrielle Bueno <drihbueno22@gmail.com>
2023-10-16 11:24:38 -0300

    atualização

--- cypress/support/page-objects/enderecoPage.js
+++ cypress/support/page-objects/enderecoPage.js
@@ -1,13 +1,16 @@
 class EnderecoPage {
 
     editarEnderecoFaturamento() {
-
         cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
         cy.get(':nth-child(1) > .title > .edit').click()
 
         cy.get('#billing_first_name').clear().type('Adrielle')
         cy.get('#billing_last_name').clear().type('Qualidade')
         cy.get('#billing_company').clear().type('Ebac')
+
+        cy.get('#select2-billing_country-container').clickk().type('Brasil').select()
+
+
 
 
     }
